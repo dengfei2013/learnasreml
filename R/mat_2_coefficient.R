008e8ebbe33e75d7db3ca06317436f6202d01b3f
dengfei2013 <dengfei_2013@163.com>
2018-12-12 14:47:22 +0800

    add a new function

---
+++ R/mat_2_coefficient.R
@@ -0,0 +1,25 @@
+#' @title Change the A or G matrix to the coefficient data
+#'
+#' @description
+#' \code{mat_2_coefficient} can Calculate coefficient form the A or G matrix.
+#' @param A_mat the matrix form the pedigree or SNP
+#' @return Result that contains thre columns: row, col, y
+#' @author Dengfei <dengfei_2013@163.com>
+#' @examples
+#' 1+1
+
+mat_2_coefficient = function(A_mat){
+  n = dim(A_mat)[1]
+  re = matrix(0,n,n)
+  for( a in 1:n){
+    for(b in a:n){
+      re[a,b] = A_mat[a,b]/sqrt(A_mat[a,a]*A_mat[b,b])
+      re[b,a] = re[a,b]
+    }
+  }
+  row.names(re) = colnames(re) = row.names(A)
+  id = row.names(re)
+  n = dim(re)[1]
+  result = data.frame(row = rep(id,each=n),col = rep(id,n),y = as.vector(re))
+  return(result)
+}
