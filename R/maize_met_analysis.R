04ed19a80943167b7c5d6e7e4c495d28c3a19c74
dengfei2013 <dengfei_2013@163.com>
2019-04-16 15:05:54 +0800

    add cv

--- R/maize_met_analysis.R
+++ R/maize_met_analysis.R
@@ -10,21 +10,25 @@
 #' head(maize)
 #' dat = maize
 #' head(dat)
-#' re = maize_met_analysis(dat)
+#' re = maize_met_analysis(dat,json=F)
 #' re
+#' maize_met_analysis(dat)
 
 
 
-maize_met_analysis = function(dat){
+maize_met_analysis = function(dat,json=TRUE){
   require(rjson)
   names(dat) = c("Loc","Rep","Cul","yield")
   dat$Loc.Rep = as.factor(paste0(dat$Loc,dat$Rep))
   dat$Loc.Cul = as.factor(paste0(dat$Loc,dat$Cul))
   mod = aov(yield ~ Loc + Loc.Rep + Cul + Loc.Cul, data = dat)
   aa = anova_tab(mod)
-  # names(aa[[1]])=c("自由度","平方和","均方","F值","p值")
-  # rownames(aa[[1]]) = c("地点","地点内重复","品种","地点:品种","残差","总和")
-  # attr(aa,"na.action")=NULL
+
+  nn = length(aa[[1]][,3])
+  mse = aa[[1]][,3][(nn-1)]
+  mean = mean(dat$yield,na.rm=TRUE)
+  cv = round(sqrt(mse)*100/mean,4)
+
   cul_lsd = LSD_test(mod,"Cul")$groups
   cul_lsd$Name = rownames(cul_lsd)
   cul_lsd = cul_lsd[,c(3,1,2)]
@@ -36,13 +40,17 @@ maize_met_analysis = function(dat){
   loc.cul_lsd = LSD_test(mod,"Loc.Cul")$groups
   loc.cul_lsd$Name = rownames(loc.cul_lsd)
   loc.cul_lsd = loc.cul_lsd[,c(3,1,2)]
-  re = list(aa,cul_lsd,loc_lsd,loc.cul_lsd)
-  aa1 = toJSON(aa)
-  cul_lsd1 = toJSON(cul_lsd)
-  loc_lsd1 = toJSON(loc_lsd)
-  loc.cul_lsd1 = toJSON(loc.cul_lsd)
-  re = c(aa1,cul_lsd1,loc_lsd1,loc.cul_lsd1)
-  # names(re) = c("anova","cul_lsd","loc_lsd","loc.cul_lsd")
-  names(re) = c("anova1","cul_lsd1","loc_lsd1","loc.cul_lsd1")
+  if(json){
+    aa1 = toJSON(aa)
+    cul_lsd1 = toJSON(cul_lsd)
+    loc_lsd1 = toJSON(loc_lsd)
+    loc.cul_lsd1 = toJSON(loc.cul_lsd)
+    cv1 = toJSON(cv)
+    re = list(aa1,cul_lsd1,loc_lsd1,loc.cul_lsd1,cv1)
+    names(re) = c("anova","cul_lsd","loc_lsd","loc.cul_lsd","cv")
+  }else{
+    re = list(aa,cul_lsd,loc_lsd,loc.cul_lsd,cv)
+    names(re) = c("anova","cul_lsd","loc_lsd","loc.cul_lsd","cv")
+  }
   return(re)
 }
