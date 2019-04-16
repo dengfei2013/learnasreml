4f62aaa63c68e9c9e74f2e52f61fa44f7df64f0d
dengfei2013 <dengfei_2013@163.com>
2019-04-16 09:34:07 +0800

    trans to json

--- R/maize_met_analysis.R
+++ R/maize_met_analysis.R
@@ -8,24 +8,33 @@
 #' library(learnasreml)
 #' data("maize")
 #' head(maize)
-#' re = maize_met_analysis(maize)
+#' dat = maize
+#' head(dat)
+#' re = maize_met_analysis(dat)
 #' re
 #' re$anova
 
 
 maize_met_analysis = function(dat){
+  require(rjson)
   names(dat) = c("Loc","Rep","Cul","yield")
   dat$Loc.Rep = as.factor(paste0(dat$Loc,dat$Rep))
   dat$Loc.Cul = as.factor(paste0(dat$Loc,dat$Cul))
   mod = aov(yield ~ Loc + Loc.Rep + Cul + Loc.Cul, data = dat)
   aa = anova_tab(mod)
-  names(aa[[1]])=c("自由度","平方和","均方","F值","p值")
-  rownames(aa[[1]]) = c("地点","地点内重复","品种","地点:品种","残差","总和")
-  attr(aa,"na.action")=NULL
+  # names(aa[[1]])=c("自由度","平方和","均方","F值","p值")
+  # rownames(aa[[1]]) = c("地点","地点内重复","品种","地点:品种","残差","总和")
+  # attr(aa,"na.action")=NULL
   cul_lsd = LSD_test(mod,"Cul")$groups
   loc_lsd = LSD_test(mod,"Loc")$groups
   loc.cul_lsd = LSD_test(mod,"Loc.Cul")$groups
   re = list(aa,cul_lsd,loc_lsd,loc.cul_lsd)
-  names(re) = c("anova","cul_lsd","loc_lsd","loc.cul_lsd")
+  aa1 = toJSON(aa)
+  cul_lsd1 = toJSON(cul_lsd)
+  loc_lsd1 = toJSON(loc_lsd)
+  loc.cul_lsd1 = toJSON(loc.cul_lsd)
+  re = c(aa1,cul_lsd1,loc_lsd1,loc.cul_lsd1)
+  # names(re) = c("anova","cul_lsd","loc_lsd","loc.cul_lsd")
+  names(re) = c("anova1","cul_lsd1","loc_lsd1","loc.cul_lsd1")
   return(re)
 }
