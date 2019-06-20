6bb45e5864f2aa32665f491bae7243328ea882df
dengfei2013 <dengfei_2013@163.com>
2019-06-20 15:37:50 +0800

    clean

--- R/china_map.R
+++ R/china_map.R
@@ -21,36 +21,3 @@ china_map = function(loc,dd){
 }
 
 
-
-# library(learnasreml)
-# data("map_dat")
-# head(map_dat)
-#
-# aa = map_dat$NAME
-# aa = as.factor(aa)
-# bb = levels(aa)
-# dd = fread("d:/chanliang.csv")
-# dongbei = dat[(NAME %in% c("黑龙江省","辽宁省","吉林省"))]
-# # g1 = ggplot(dongbei,aes(x = long,y=lat,group=group,fill=NAME)) + geom_polygon(fill = "beige") + geom_path(colour = "grey40")
-# g1 = ggplot(data = dat) +
-#   geom_polygon(aes(x = long, y = lat, fill = NAME, group = group),fill="grey" ,color = "white") +
-#   coord_fixed(1.3) +guides(fill=FALSE)  # do this to leave off the color legend
-# g1
-#
-# tt = dd
-# min = min(tt$value)-0.0001
-# a25=quantile(tt$value)[2]
-# a50=quantile(tt$value)[3]
-# a75=quantile(tt$value)[4]
-# max = max(tt$value)+0.0001
-#
-# # g1 + geom_point(data = tt, aes(x = tt$longtitude, y = tt$latitude,
-# #                                colour=cut(tt$value,c(min,a25,a50,a75,max),size=5)))+
-# #   scale_color_manual(name = "qsec",
-# #                    values = c("(min,a25]" = "black",
-# #                               "(a25,a50]" = "blue",
-# #                               "(a50,a75]" = "yellow",
-# #                               "(a75, max]" = "red"))
-# g1 + geom_point(data=tt,aes(x = tt$longtitude,y=tt$latitude,colour=tt$value))
-
-
