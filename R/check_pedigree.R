7a7d19d9872eedb455f2d8ef5e1c71efe3ddd497
dengfei2013 <dengfei_2013@163.com>
2019-07-24 09:21:04 +0800

    fixed a bug

--- R/check_pedigree.R
+++ R/check_pedigree.R
@@ -13,39 +13,44 @@
 
 
 check_pedigree <- function(ped,dat=NULL){
-  require(tidyverse)
-  ped = as.data.frame(ped)
-  ped[ped==0] = NA
-  id = ped[,1]
-  id = id[!is.na(id)]
-  sire = ped[,2]
-  sire = sire[!is.na(sire)]
-  dam = ped[,3]
-  dam = dam[!is.na(dam)]
-  total = c(id,sire,dam) %>% sort %>% unique()
-  id_dup = id[duplicated(id)]
-  inte = intersect(sire,dam)
-  a1 = cat("系谱共有行数:", dim(ped)[1],"\n")
-  a2 = cat("个体共有个数:",length(unique(sort(id))),"\n")
-  a3 = cat("父本共有个数:", length(unique(sort(sire))),"\n")
-  a4 = cat("母本共有个数:", length(unique(sort(dam))),"\n")
-  if(length(id_dup)==0){
-    a5 = cat("个体没有重复！\n")
+  if(dim(ped)[2] != 3){
+    stop("系谱数据需要三列！")
   }else{
-    a5 = cat("个体重复数为:", length(id_dup),"个,分别是:",id_dup,"\n")
+    require(tidyverse)
+    ped = as.data.frame(ped)
+    ped[ped==0] = NA
+    id = ped[,1]
+    id = id[!is.na(id)]
+    sire = ped[,2]
+    sire = sire[!is.na(sire)]
+    dam = ped[,3]
+    dam = dam[!is.na(dam)]
+    total = c(id,sire,dam) %>% sort %>% unique()
+    id_dup = id[duplicated(id)]
+    inte = intersect(sire,dam)
+    a1 = cat("系谱共有行数:", dim(ped)[1],"\n")
+    a2 = cat("个体共有个数:",length(unique(sort(id))),"\n")
+    a3 = cat("父本共有个数:", length(unique(sort(sire))),"\n")
+    a4 = cat("母本共有个数:", length(unique(sort(dam))),"\n")
+    if(length(id_dup)==0){
+      a5 = cat("个体没有重复！\n")
+    }else{
+      a5 = cat("个体重复数为:", length(id_dup),"个,分别是:",id_dup,"\n")
 
-  }
-  if(length(inte)==0){
-    a6 = cat("父母本个体没有交叉！\n")
-  }else{
-    a6 = cat("父母本交叉个数为:",length(inte),"个,分别是:",inte,"\n")
+    }
+    if(length(inte)==0){
+      a6 = cat("父母本个体没有交叉！\n")
+    }else{
+      a6 = cat("父母本交叉个数为:",length(inte),"个,分别是:",inte,"\n")
 
+    }
+    if(!is.null(dat)){
+      dat = as.data.frame(dat)
+      id_dat = dat[,1]
+      a7 = cat("有表型无系谱个体为：",length(setdiff(id_dat,total)),"个,分别是:",setdiff(id_dat,total),"\n")
+      re = setdiff(id_dat,total)
+      return(re)
+    }
   }
-  if(!is.null(dat)){
-    dat = as.data.frame(dat)
-    id_dat = dat[,1]
-    a7 = cat("有表型无系谱个体为：",length(setdiff(id_dat,total)),"个,分别是:",setdiff(id_dat,total),"\n")
-    re = setdiff(id_dat,total)
-    return(re)
-  }
+
 }
